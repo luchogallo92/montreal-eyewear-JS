de6b21a920418f5314e5b6d382ad7a8a8a5f7178
luchogallo92 <lucianogallo.di@gmail.com>
2023-06-16 17:25:28 -0300

    1.0

---
+++ main.js
@@ -0,0 +1,21 @@
+// Array de productos
+
+const productos_anteojos = [
+    {
+        Id: 1,
+        nombre: "Polaroid Ibiza",
+        Precio: "33.000",
+        Categoria: "Sol",
+    },
+
+    {
+        Id: 2,
+        nombre: "Polaroid Ibiza",
+        Precio: "33.000",
+        Categoria: "Sol",
+    },
+
+
+];
+
+console.dir(productos_anteojos);
