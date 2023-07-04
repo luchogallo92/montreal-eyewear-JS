50095631a71c56413244d61317e1e537084d6a5b
luchogallo92 <lucianogallo.di@gmail.com>
2023-07-04 10:54:15 -0300

    1.1

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 const darkmode = document.getElementById ("toggle-dark-mode");
 
 darkmode.addEventListener ('click', () =>{
-    document.body.classList.toggle('dark-mode')
+    document.body.classList.toggle("dark-mode")
 
     if(document.body.classList.contains ("dark-mode")){
         darkmode.src = ("./Assets/Iconos/sol.png")
@@ -12,7 +12,9 @@ darkmode.addEventListener ('click', () =>{
     }
 });
 
-const darkmodebackground = document.getElementById
+
+
+
 ///////
 const cardanteojos = document.querySelector (".boxes-shop");
 
