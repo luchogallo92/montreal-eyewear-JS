3917a41f9edb31fe09e9b3e1ad8071a90fd2bbbd
Luciano Gallo <lucianogallo.di@gmail.com>
2023-07-03 22:14:21 -0300

    1.1

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
+
+//Dark mode//
+const darkmode = document.getElementById ("toggle-dark-mode");
+
+darkmode.addEventListener ('click', () =>{
+    document.body.classList.toggle('dark-mode')
+
+    if(document.body.classList.contains ("dark-mode")){
+        darkmode.src = ("./Assets/Iconos/sol.png")
+    } else {
+        darkmode.src = ("./Assets/Iconos/darkmode.png")
+    }
+});
+
+const darkmodebackground = document.getElementById
+///////
 const cardanteojos = document.querySelector (".boxes-shop");
 
